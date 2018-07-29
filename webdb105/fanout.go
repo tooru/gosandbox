bfe56155dacb94a451ef7405b662b0adb03c6fea
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-07-30 08:56:57 +0900

    Modified fanout.go.

--- webdb105/fanout.go
+++ webdb105/fanout.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"time"
 )
 
 const nMessages = 6
 
-func main() {
+func fanout(message string, propagate func(context.Context, <-chan string, chan<- string, chan<- string, chan<- string) error) {
+	fmt.Printf("%s:\n", message)
+
 	var src = make(chan string)
 	var dst1 = make(chan string)
 	var dst2 = make(chan string)
@@ -22,6 +25,7 @@ func main() {
 				done <- nil
 				return
 			}
+			time.Sleep(200 * time.Millisecond)
 			fmt.Println("dst1:" + d)
 		}
 	}()
@@ -32,6 +36,7 @@ func main() {
 				done <- nil
 				return
 			}
+			time.Sleep(100 * time.Millisecond)
 			fmt.Println("dst2:" + d)
 		}
 	}()
@@ -42,6 +47,7 @@ func main() {
 				done <- nil
 				return
 			}
+			time.Sleep(50 * time.Millisecond)
 			fmt.Println("dst3:" + d)
 		}
 
@@ -59,7 +65,7 @@ func main() {
 		cancel()
 	}()
 
-	err := call(ctx, src, dst1, dst2, dst3)
+	err := propagate(ctx, src, dst1, dst2, dst3)
 	if err != nil {
 		panic(err)
 	}
@@ -72,13 +78,65 @@ func main() {
 	}
 }
 
-func call(ctx context.Context, src chan string, dst1 chan string, dst2 chan string, dst3 chan string) error {
+func propagateSingle(ctx context.Context, src <-chan string, dst1 chan<- string, dst2 chan<- string, dst3 chan<- string) error {
 	for {
 		data, ok := <-src
 		if !ok {
 			return nil
 		}
-		var channels = []chan string{dst1, dst2, dst3}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case dst1 <- data:
+		case dst2 <- data:
+		case dst3 <- data:
+		}
+	}
+}
+func propagateMulti(ctx context.Context, src <-chan string, dst1 chan<- string, dst2 chan<- string, dst3 chan<- string) error {
+	for {
+		data, ok := <-src
+		if !ok {
+			return nil
+		}
+
+		for i := 0; i < 3; i++ {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case dst1 <- data:
+			case dst2 <- data:
+			case dst3 <- data:
+			}
+		}
+	}
+}
+func propagateMultiByLoop(ctx context.Context, src <-chan string, dst1 chan<- string, dst2 chan<- string, dst3 chan<- string) error {
+	for {
+		data, ok := <-src
+		if !ok {
+			return nil
+		}
+		var channels = []chan<- string{dst1, dst2, dst3}
+
+		for _, ch := range channels {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case ch <- data:
+			}
+		}
+	}
+}
+func propagateMultiByReflection(ctx context.Context, src <-chan string, dst1 chan<- string, dst2 chan<- string, dst3 chan<- string) error {
+	for {
+		data, ok := <-src
+		if !ok {
+			return nil
+		}
+		var channels = []chan<- string{dst1, dst2, dst3}
+
 		var cases = make([]reflect.SelectCase, len(channels))
 
 		for i, ch := range channels {
@@ -93,4 +151,11 @@ func call(ctx context.Context, src chan string, dst1 chan string, dst2 chan stri
 			cases = append(cases[:chosen], cases[chosen+1:]...)
 		}
 	}
+}
+
+func main() {
+	fanout("propagateSingle", propagateSingle)
+	fanout("propagateMulti", propagateMulti)
+	fanout("propagateMultiByLoop", propagateMultiByLoop)
+	fanout("propagateMultiByReflection", propagateMultiByReflection)
 }
