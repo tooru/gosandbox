ea9f798151585fac8591ab6d30e6e2994c9edbd8
Tooru Takahashi <tooru.takahashi134@gmail.com>
2018-12-01 14:37:19 +0900

    Added main.go.

---
+++ cingo/3.1-goroutinesize/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() {
+		wg.Done()
+		<-c
+	}
+	const numGroutines = 1e4
+	wg.Add(numGroutines)
+	before := memConsumed()
+	for i := numGroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb\n", float64(after-before)/numGroutines/1000)
+}
