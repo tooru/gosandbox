5b90d1d454a79a2552ef12d8dd096fb8f0cf75c5
Tooru Takahashi <tooru.takahashi134@gmail.com>
2019-03-16 23:31:33 +0900

    added ssarule

---
+++ ssarule/example/append.go
@@ -0,0 +1,7 @@
+package example
+
+func main() {
+	var xs []rune
+	xs = append(xs, 'a')
+	xs = append(xs, 'b')
+}
---
+++ ssarule/example/fma.go
@@ -0,0 +1,5 @@
+package example
+
+func fusedMulAdd(a, b, c float64) float64 {
+	return a*c + b
+}
